e3295dd1930a3b908692b28c0c6e17517c40ea57
JulieRaph <julie.tolve@gmail.com>
2025-03-31 14:57:49 +0200

    Add createInt8TypedArray function to create and validate Int8 typed arrays

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,1 +1,14 @@
-
+export default function createInt8TypedArray(length, position, value) {
+  const buffer = new ArrayBuffer(length);
+  const int8Array = new Int8Array(buffer);
+  const dataView = new DataView(buffer);
+  if (position < 0 || position >= length) {
+    throw new Error('Position outside range');
+  }
+  int8Array[position] = value;
+  const result = dataView.getInt8(position);
+  if (result !== value) {
+    throw new Error('Value not set');
+  }
+  return dataView;
+}
