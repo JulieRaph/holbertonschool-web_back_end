817a5e1b9655d88ddea9f6c094582508047ebd6e
JulieRaph <julie.tolve@gmail.com>
2025-03-31 12:59:41 +0200

    Add updateStudentGradeByCity function to update student grades based on location

---
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,13 @@
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  if (!Array.isArray(students) || !Array.isArray(newGrades)) {
+    return [];
+  }
+  return students.filter((studentList) => studentList.location === city)
+    .map((studentList) => {
+      const studentIdGrade = newGrades.find((grade) => grade.studentId === studentList.id);
+      return {
+        ...studentList,
+        grade: studentIdGrade ? studentIdGrade.grade : 'N/A',
+      };
+    });
+}
