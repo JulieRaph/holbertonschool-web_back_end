5c0db18c3e3ca1a2c7ddeecf2560f732ef9f690c
JulieRaph <julie.tolve@gmail.com>
2025-04-01 12:40:20 +0200

    Add handleResponseFromAPI function to manage API promise responses

---
+++ ES6_promise/2-then.js
@@ -0,0 +1,17 @@
+export default function handleResponseFromAPI(promise) {
+  return new Promise((resolve, reject) => {
+    promise
+      .then(() => {
+        resolve({
+          status: 200,
+          body: 'Success',
+        });
+      })
+      .catch((error) => {
+        reject(error);
+      })
+      .finally(() => {
+        console.log('Got a response from the API');
+      });
+  });
+}
