5ed33961be1d87604b9d94cb75a1f478e3b3cce2
JulieRaph <julie.tolve@gmail.com>
2025-04-01 17:43:03 +0200

    Refactor divideFunction to throw an error for division by zero and simplify promise handling

--- ES6_promise/8-try.js
+++ ES6_promise/8-try.js
@@ -1,9 +1,6 @@
-/* eslint-disable */
 export default function divideFunction(numerator, denominator) {
-  return new Promise((resolve, reject) => {
-    if (denominator === 0) {
-      return reject(new Error('cannot divide by 0'));
-    }
-    resolve(numerator / denominator);
-  });
+  if (denominator === 0) {
+    throw new Error('cannot divide by 0');
+  }
+  return (numerator / denominator);
 }
