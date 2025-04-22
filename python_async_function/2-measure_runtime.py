ba582a5599fef2dcaa94987531a95fdb533175d1
JulieRaph <julie.tolve@gmail.com>
2025-04-22 16:42:39 +0200

    Refactor measure_time function to encapsulate async logic within a nested function

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -12,12 +12,15 @@ import time
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-async def measure_time(n: int, max_delay: int) -> float:
+def measure_time(n: int, max_delay: int) -> float:
     """
     function to measure the average time
     """
-    start_time = time.time()
-    await wait_n(n, max_delay)
-    end_time = time.time()
-    total_time = end_time - start_time
-    return total_time / n
+    async def async_time_measure():
+
+        start_time = time.time()
+        await wait_n(n, max_delay)
+        end_time = time.time()
+        total_time = end_time - start_time
+        return total_time / n
+    return asyncio.run(async_time_measure())
