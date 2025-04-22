e32fe8467118b6e00f6c0cc8425d1609cee01551
JulieRaph <julie.tolve@gmail.com>
2025-04-22 11:09:57 +0200

    Fix type hint for input_list parameter in sum_list function

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -2,8 +2,9 @@
 """
 Module providing a function that take a list and return the sum as a float
 """
+from typing import List
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """Function to return the sum of the float"""
     return sum(input_list)
