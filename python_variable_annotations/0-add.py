d2cc77f8f4ca90aaa30f0f792a9d80236c9593a0
JulieRaph <julie.tolve@gmail.com>
2025-04-22 10:02:36 +0200

    Add a blank line for improved readability in the add module

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Module providing a function to add two numbers"""
 
+
 def add(a: float, b: float) -> float:
     """Function to add two numbers and return their sum(float)"""
     return a + b
