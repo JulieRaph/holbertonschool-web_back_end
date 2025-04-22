440c4a445ef0c6bd1af5dec57c8d818afe772408
JulieRaph <julie.tolve@gmail.com>
2025-04-22 16:31:47 +0200

    Add measure_time function to calculate average execution time of wait_n

---
+++ python_async_function/2-measure_runtime.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+Module to create a function that measures
+how long wait_n takes to execute, then divides
+this total time by n to obtain the average
+time per call
+"""
+
+import asyncio
+import time
+
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+async def measure_time(n: int, max_delay: int) -> float:
+    """
+    function to measure the average time
+    """
+    start_time = time.time()
+    await wait_n(n, max_delay)
+    end_time = time.time()
+    total_time = end_time - start_time
+    return total_time / n
