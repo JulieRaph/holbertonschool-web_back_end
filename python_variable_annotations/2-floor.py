fb59d391a474e29a47e475cc33278996689e89a3
JulieRaph <julie.tolve@gmail.com>
2025-04-22 10:13:34 +0200

    Add floor module with function to return the floor of a float

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+Module providing a function that take a float and return the floor of the float
+"""
+
+
+def floor(n: float) -> float:
+    """Function that return the floor of the float"""
+    return(n)
