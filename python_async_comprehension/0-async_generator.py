77329877bebff76b46d00d7fc258a9477daeda07
JulieRaph <julie.tolve@gmail.com>
2025-04-28 13:23:41 +0200

    Add async_generator coroutine to the python_async_comprehension module

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Module providing a coroutine called async_generator that takes no arguments,
+and will loop 10 times each time asynchronously wait 1 second
+"""
+
+import asyncio
+import random
+from typing import AsyncGenerator
+
+
+async def async_generator() -> AsyncGenerator[float, None]:
+    """Function to loop 10 times, wait 1 second, and yield a random number"""
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
