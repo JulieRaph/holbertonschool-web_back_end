d2d2cfffc3411f1f2a56ca873be3792332f0b2f5
JulieRaph <julie.tolve@gmail.com>
2025-05-19 16:44:30 +0200

    Add HTTP server implementation in 5-http.js

---
+++ Node_JS_basic/5-http.js
@@ -0,0 +1,28 @@
+// This module to create a small HTTP server using the http module
+
+const http = require('http'); // Import the http module
+const countStudents = require('./3-read_file_async'); // Import the countStudents function from the 3-read_file_async module
+
+const app = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-type', 'text/plain');
+
+  if (req.url === '/') {
+    res.end('Hello Holberton School!');
+  } else if (req.url === '/students') {
+    res.write('This is the list of our students\n');
+    countStudents(process.argv[2])
+      .then((data) => {
+        res.end(data); // data is assumed to be a pre-formatted string
+      })
+      .catch((error) => {
+        res.end(error.message); // error.message is appended after the initial write
+      });
+  } else {
+    res.statusCode = 404;
+    res.end('Not found');
+  }
+});
+app.listen(1245); // app.listen is now a function call
+
+module.exports = app;
