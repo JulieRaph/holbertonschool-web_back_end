be7c48d971309e5512712e82e3e4b4e8c78ce6a4
JulieRaph <julie.tolve@gmail.com>
2025-05-19 18:26:55 +0200

    Fix formatting of welcome message in 1-stdin.js

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -2,7 +2,7 @@
 // using the process module
 // The program should use the process.stdin and process.stdout objects to read and write data
 
-process.stdout.write('Welcome to Holberton School, what is your name? \n');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('readable', () => {
   const name = process.stdin.read();
