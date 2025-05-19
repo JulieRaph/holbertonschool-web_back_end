8e73a27f8f8af3fbd8b73348c60afd8d573eb80f
JulieRaph <julie.tolve@gmail.com>
2025-05-19 18:15:12 +0200

    Implement basic HTTP server using Express framework

--- Node_JS_basic/6-http_express.js
+++ Node_JS_basic/6-http_express.js
@@ -1,3 +1,11 @@
 // This module to create a small HTTP server using Express module
 
 const express = require('express'); // Import the express module
+
+const app = express(); // Create an instance of express
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!'); // Send a response to the client
+});
+app.listen(1245); // Start the server on port 1245
+
+module.exports = app; // Export the app instance
