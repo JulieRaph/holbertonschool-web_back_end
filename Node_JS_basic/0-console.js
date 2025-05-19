2f3708de2108e74de9102423e2a6ef09440b7b77
JulieRaph <julie.tolve@gmail.com>
2025-05-19 17:51:45 +0200

    ntm

--- Node_JS_basic/0-console.js
+++ Node_JS_basic/0-console.js
@@ -1,5 +1,5 @@
 // Use the function displayMessage to print a message to the console
 const displayMessage = (message) => {
-  process.stdout.write(`${message}\n`);
+  process.stdout.write(`${message}`);
 };
 module.exports = displayMessage;
