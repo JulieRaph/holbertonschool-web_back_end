547c2eab58d67f7c73787af0b564bf3fb9eb4e93
JulieRaph <julie.tolve@gmail.com>
2025-05-19 18:33:20 +0200

    Change closing message to use console.log for consistency

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -12,5 +12,5 @@ process.stdin.on('readable', () => {
 });
 
 process.stdin.on('end', () => {
-  process.stdout.write('This important software is now closing');
+  console.log('This important software is now closing');
 });
