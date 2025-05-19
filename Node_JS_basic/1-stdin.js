4b287e08e25d8510988daf5ab37f473acd92c7de
JulieRaph <julie.tolve@gmail.com>
2025-05-19 13:49:20 +0200

    Add stdin module to read user input and display name

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,16 @@
+// This module to create a program that reads from standard input and writes to standard output
+// using the process module
+// The program should use the process.stdin and process.stdout objects to read and write data
+
+process.stdout.write('Welcome to Holberton School, what is your name? \n');
+
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  if (name !== null) {
+    process.stdout.write(`Your name is: ${name}`);
+  }
+});
+
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
+});
