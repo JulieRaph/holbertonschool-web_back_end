e4632cebb174ab40acec6035b9c1687943b09f53
JulieRaph <julie.tolve@gmail.com>
2025-05-19 17:55:04 +0200

    Replace process.stdout.write with console.log in displayMessage function

--- Node_JS_basic/0-console.js
+++ Node_JS_basic/0-console.js
@@ -1,5 +1,5 @@
 // Use the function displayMessage to print a message to the console
 const displayMessage = (message) => {
-  process.stdout.write(`${message}`);
+  console.log(message);
 };
 module.exports = displayMessage;
