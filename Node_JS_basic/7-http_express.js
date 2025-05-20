367285fa0356fc2207c5d8a8a7ffbd0a2bbea594
JulieRaph <julie.tolve@gmail.com>
2025-05-20 11:01:41 +0200

    Refactor HTTP server response formatting for consistency

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -5,28 +5,28 @@ const countStudents = require('./3-read_file_async');
 
 const app = express();
 app.get('/', (req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-type', 'text/plain');
-    res.send('Hello Holberton School!');
+  res.statusCode = 200;
+  res.setHeader('Content-type', 'text/plain');
+  res.send('Hello Holberton School!');
 });
 app.get('/students', (req, res) => {
-    countStudents(process.argv[2])
-        .then((data) => {
-        res.statusCode = 200;
-        res.setHeader('Content-type', 'text/plain');
-        res.send('This is the list of our students\n');
-        })
-        .catch((error) => {
-            res.statusCode = 500;
-            res.setHeader('Content-type', 'text/plain');
-            res.send(`This is the list of our students\n${error.message}`);
-        })
+  countStudents(process.argv[2])
+    .then((data) => {
+      res.statusCode = 200;
+      res.setHeader('Content-type', 'text/plain');
+      res.send('This is the list of our students\n');
+    })
+    .catch((error) => {
+      res.statusCode = 500;
+      res.setHeader('Content-type', 'text/plain');
+      res.send(`This is the list of our students\n${error.message}`);
     });
+});
 app.use((req, res) => {
-    res.statusCode = 404;
-    res.setHeader('Content-type', 'text/plain');
-    res.send('Not found');
+  res.statusCode = 404;
+  res.setHeader('Content-type', 'text/plain');
+  res.send('Not found');
 });
 app.listen(1245);
 
-module.exports = app; 
+module.exports = app;
