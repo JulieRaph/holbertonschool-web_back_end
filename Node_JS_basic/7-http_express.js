d76bd61c429109c05a982593098f3fd6dbfb1b98
JulieRaph <julie.tolve@gmail.com>
2025-05-20 10:56:04 +0200

    Add ESLint configuration and implement HTTP server with student listing

---
+++ Node_JS_basic/7-http_express.js
@@ -0,0 +1,32 @@
+// This module to create a small HTTP server using Express module
+
+const express = require('express');
+const countStudents = require('./3-read_file_async');
+
+const app = express();
+app.get('/', (req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-type', 'text/plain');
+    res.send('Hello Holberton School!');
+});
+app.get('/students', (req, res) => {
+    countStudents(process.argv[2])
+        .then((data) => {
+        res.statusCode = 200;
+        res.setHeader('Content-type', 'text/plain');
+        res.send('This is the list of our students\n');
+        })
+        .catch((error) => {
+            res.statusCode = 500;
+            res.setHeader('Content-type', 'text/plain');
+            res.send(`This is the list of our students\n${error.message}`);
+        })
+    });
+app.use((req, res) => {
+    res.statusCode = 404;
+    res.setHeader('Content-type', 'text/plain');
+    res.send('Not found');
+});
+app.listen(1245);
+
+module.exports = app; 
