3ec8acbccc8655fe31b13ee73ffdcd013d9623cf
JulieRaph <julie.tolve@gmail.com>
2025-05-22 11:26:17 +0200

    Refactor: Improve output logging and handle empty lines in countStudents function

--- Node_JS_basic/3-read_file_async.js
+++ Node_JS_basic/3-read_file_async.js
@@ -1,4 +1,6 @@
-const fs = require('fs').promises; // For TEST
+// This module to create a program that reads from a file async as csv and writes to standard output
+
+const fs = require('fs').promises;
 
 const countStudents = async (filePath) => {
   try {
@@ -10,13 +12,16 @@ const countStudents = async (filePath) => {
     }
 
     const students = lines.slice(1).map((line) => {
+      if (line.trim() === '') {
+        return null;
+      }
       const [firstname, lastname, age, field] = line.split(',');
       return {
         firstname, lastname, age, field,
       };
-    });
+    }).filter(student => student !== null);
 
-    let output = `Number of students: ${students.length}\n`;
+    console.log(`Number of students: ${students.length}`);
 
     const fields = students.reduce((acc, student) => {
       if (!acc[student.field]) {
@@ -28,10 +33,17 @@ const countStudents = async (filePath) => {
 
     for (const field in fields) {
       if (Object.prototype.hasOwnProperty.call(fields, field)) {
-        output += `Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}\n`;
+        console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
       }
     }
 
+    let output = `Number of students: ${students.length}\n`;
+    for (const field in fields) {
+      if (Object.prototype.hasOwnProperty.call(fields, field)) {
+        output += `Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}\n`;
+      }
+    }
+    
     return output.trim();
   } catch (error) {
     throw new Error('Cannot load the database');
