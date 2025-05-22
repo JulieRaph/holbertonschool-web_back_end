18cbeaaf8dae5f07aef8aa78a20e781e26b1288c
JulieRaph <julie.tolve@gmail.com>
2025-05-22 11:25:31 +0200

    Refactor: Improve comments and formatting in HTTP server setup

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,12 +1,12 @@
-// This module that create a small HTTP server using the http module
-
-const http = require('http'); // Import the http module
+// Create a simple HTTP server using Node's http module ok
+const http = require('http');
 
 const app = http.createServer((req, res) => {
-  res.statusCode = 200; // Set the status code to 200
-  res.setHeader('Content-type', 'text/plain'); // Set the content type to text/plain
-  res.end('Hello Holberton School!'); // End the response with a message
-}); // Create the server
-app.listen = 1245;
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello Holberton School!');
+});
 
-module.exports = app; // Export the app
+app.listen(1245);
+
+module.exports = app;
